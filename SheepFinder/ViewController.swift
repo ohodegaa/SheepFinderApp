dc23aa6579ca67e4e2085888c1bff9116e0bc491
Ole Håkon Ødegaard <ohodegaa@stud.ntnu.no>
2020-01-23 16:11:52 +0100

    Initial Commit

---
+++ SheepFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SheepFinder
+//
+//  Created by Ole Håkon Ødegaard on 23/01/2020.
+//  Copyright © 2020 Ole Håkon Ødegaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
